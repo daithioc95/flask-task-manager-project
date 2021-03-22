6947afdbc22d79e1aea7a4fbb54c03fbfddc3b28
daithioc95 <daithioc@outlook.com>
2021-03-22 15:38:38 +0000

    Add custom Materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -11,5 +11,38 @@ $(document).ready(function () {
             done: "Select"
         }
     });
-
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        // variables for valid/invalid css styles
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        // to ensure elements are physically on the DOM
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        // traversing elements on DOM
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            // if list item is selected & doesn't have disabled class
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    // Apply green
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            // if either valid or classInvalid, apply green class
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                // if bottom border isn't updated to green, proper selection not made
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
 });
